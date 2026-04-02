97412850528c0554564accb3a9d0f7a0cc5b3627
Oshiro20 <166664320+Oshiro20@users.noreply.github.com>
2026-04-02 14:21:21 -0500

    feat: export wallet summary for MyLifeOS integration on app start

--- lib/app.dart
+++ lib/app.dart
@@ -3,8 +3,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'l10n/app_localizations.dart';
 import 'core/router/app_router.dart';
+import 'core/services/wallet_summary_service.dart';
 import 'data/datasources/recurrence_service.dart';
 import 'data/datasources/keep_alive_service.dart';
+import 'data/database/drift_database.dart';
 import 'presentation/providers/theme_provider.dart';
 
 class MyApp extends ConsumerStatefulWidget {
@@ -20,6 +22,8 @@ class _MyAppState extends ConsumerState<MyApp> {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       ref.read(recurrenceServiceProvider).checkDuePayments();
+      // Exportar resumen para MyLifeOS (silencioso, no bloquea UI)
+      WalletSummaryService(AppDatabase()).exportSummary();
     });
     // Iniciar keep-alive para despertar backend
     keepAliveService.start();
